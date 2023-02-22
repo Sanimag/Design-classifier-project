eeb9c33037a68a0c84b373bcb875b1e849c35914
Sanimag <mr.otkilbaev@gmail.com>
2023-02-22 13:21:20 +0600

    Created basic structure

---
+++ Product_design_webproject/Product_design_webapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# Create your views here.
+def Main(request):
+	return render(request, 'index.html')
+
+def Result(request):
+	product_description = request.GET['product_description']
+	return render(request, 'result.html', {'description':product_description})
