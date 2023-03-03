9444b4fda4b275fc8cf96164b1c7c296be4b0e67
San <mr.otkilbaev@gmail.com>
2023-03-03 07:47:36 +0000

    Added PostgreSQL with docker as database and some changes to  models.py

--- Product_design_webproject/Product_design_webapp/models.py
+++ Product_design_webproject/Product_design_webapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    pr_title = models.CharField(max_length = 500)
+    pr_description = models.TextField()
+    pr_image = models.ImageField(upload_to='product_images/')
--- Product_design_webproject/Product_design_webproject/settings.py
+++ Product_design_webproject/Product_design_webproject/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Product_design_webproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'products',
+	'USER': 'postgres',
+	'PASSWORD': 'mypassword',
+	'HOST':'localhost'
     }
 }
 
@@ -118,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
